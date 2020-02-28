f84e036ba41fe17da71f84f895d28bbe04ff2f9d
SashaPlavi <sasha.bingulac@gmail.com>
2020-02-28 10:01:47 +0100

    back on change

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="row">
     <div class="col-md-8">
-      <input />
+      <input @change="onInput" />
     </div>
-    <div class="col-md-2">
-      <button class="btn btn-success" @click="onInput">Serch</button>
-    </div>
+    <div class="col-md-2"></div>
   </div>
 </template>
 
